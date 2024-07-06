652ca85f22429c53241292b271d9214395711681
luis antonio <tyluis20@gmail.com>
2024-07-06 18:45:25 -0300

    add cypress

---
+++ cypress/e2e/fundamentals.cy.js
@@ -0,0 +1,12 @@
+describe("fundamentals test", () => {
+  //
+
+  it("passes", () => {
+    cy.visit("https://example.cypress.io");
+  });
+  //
+
+  it("passada", () => {
+    cy.visit("http://gooogle.com");
+  });
+});
