45564db7aedc48626b5a5e554871fbaf2074dadc
luis antonio <tyluis20@gmail.com>
2024-07-06 19:23:41 -0300

    testando e escrevendo primeiro test

--- cypress/e2e/fundamentals.cy.js
+++ cypress/e2e/fundamentals.cy.js
@@ -1,12 +1,21 @@
 describe("fundamentals test", () => {
-  //
+  // it("passes", () => {
+  //   cy.visit("https://example.cypress.io");
+  // });
+  // //
+  // it("teste", () => {
+  //   cy.visit("http://gooogle.com");
+  //   cy.get();
+  //   cy.get("[elemento]").should("have.length", 3);
+  //   cy.get("form").find("input").should("not.have.class", "disabled");
+  //   cy.get(["data"]).invoke("").should("not.be.visible");
+  //   cy.get(":radio").should("have.length", 2);
+  // });
 
-  it("passes", () => {
-    cy.visit("https://example.cypress.io");
-  });
-  //
-
-  it("passada", () => {
-    cy.visit("http://gooogle.com");
+  it("visita front ", () => {
+    cy.visit("http://localhost:3000/fundamentals");
+    cy.get('[data-test="fundamentals-header"]').contains(
+      /Testing Fundamentals/i
+    );
   });
 });
